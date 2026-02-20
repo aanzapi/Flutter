00127efb0aae247d19ccb7ef6c7163cd6d973122
aanzapi <cuyaan08@gmail.com>
2026-02-20 19:44:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BOT_TOKEN: process.env.BOT_TOKEN || "8354432194:AAHBSnA2EDbQEJWikFBcD3ImPGpmxZpkE7A",
+    BOT_TOKEN: process.env.BOT_TOKEN || "7570058080:AAFXlaPKP6-mwdtxSlZ_TMf0B54XkraAl5w",
     BUILD_TIMEOUT: 1000 * 60 * 10, // 10 menit timeout
     WORK_DIR: "./builds"
 };

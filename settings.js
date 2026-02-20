1623c2924efcf99c87931bb8fcbf15d1019c18cb
aanzapi <cuyaan08@gmail.com>
2026-02-20 19:09:54 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+    BOT_TOKEN: process.env.BOT_TOKEN || "8354432194:AAHBSnA2EDbQEJWikFBcD3ImPGpmxZpkE7A",
+    BUILD_TIMEOUT: 1000 * 60 * 10, // 10 menit timeout
+    WORK_DIR: "./builds"
+};

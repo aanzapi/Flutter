08aa2f0fa4b36ad43cc64713479acae530210c0b
aanzapi <cuyaan08@gmail.com>
2026-02-20 19:56:46 +0700

    Delete settings.js

--- settings.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    BOT_TOKEN: process.env.BOT_TOKEN || "7570058080:AAFXlaPKP6-mwdtxSlZ_TMf0B54XkraAl5w",
-    BUILD_TIMEOUT: 1000 * 60 * 10, // 10 menit timeout
-    WORK_DIR: "./builds"
-};
